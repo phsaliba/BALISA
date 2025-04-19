e66fb931f47320af81c9f71f6f06baa493d3bdce
abilaS <pedroh.spr@gmail.com>
2025-04-19 22:09:15 +0200

    First iteration of 1D Mesh types and nodes

--- Mesh/Element1D.py
+++
@@ -1,5 +0,0 @@
-class Element1D():
-    def __init__(self, x_position, west_element=None, east_element=None):
-        self.x_position = x_position
-        self.west_element = west_element
-        self.east_element = east_element
