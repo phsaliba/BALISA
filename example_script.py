226a2207c9883c9031adfbc2b27e2807715e4d16
abilaS <pedroh.spr@gmail.com>
2025-04-24 22:14:20 +0200

    additional changes to example script

--- example_script.py
+++ example_script.py
@@ -2,7 +2,8 @@
 from Mesh import mesh_1d
 from Solver import boundary_conditions_1d, diffusion_equations_1d
 
-mesh = mesh_1d.create_constant_section_mesh_1d(domain_length=0.5, cross_section=0.01, n_nodes=5)
+# mesh = mesh_1d.create_constant_section_mesh_1d(domain_length=0.5, cross_section=0.01, n_nodes=5)
+mesh = mesh_1d.create_linear_section_mesh1D(domain_length=0.5, inlet_section=0.05, outlet_section=0.01, n_nodes=100)
 
 boundary_conditions = boundary_conditions_1d.steady_conduction_1d_bc(phi_west_BC=100, phi_east_BC=500)
 
