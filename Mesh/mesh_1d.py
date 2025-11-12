564f590ee0036c4532961a4346612ea3c3e97299
abilaS <pedroh.spr@gmail.com>
2025-11-12 14:55:22 -0400

    rewrote node and mesh 1d files to serve as mesh file writers for c++ solver

--- Mesh/mesh_1d.py
+++ Mesh/mesh_1d.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 from typing import List
 
 from Mesh.node_1d import Node1D
 
-def equally_spaced_nodes(domain_length, n_nodes):
+def equally_spaced_nodes(domain_length, n_nodes) -> np.array:
     dx = domain_length / n_nodes
-    return dx, np.arange(dx / 2, domain_length, dx)
+    return np.arange(dx / 2, domain_length, dx)
 
 class Mesh1D():
-    def __init__(self, nodes: List[Node1D], cross_section_west: float, cross_section_east: float, domain_length: float):
+    def __init__(self, nodes: List[Node1D], west_bc: Node1D, east_bc: Node1D):
         self.nodes = nodes
-        self.cross_section_west = cross_section_west
-        self.cross_section_east = cross_section_east
-        self.domain_length = domain_length
+        self.west_bc = west_bc
+        self.east_bc = east_bc
+        self.domain_length = east_bc.x - west_bc.x
 
     def display_mesh(self):        
         x_position_list = np.array([0., *[display_node.x for display_node in self.nodes], self.domain_length])
@@ -24,7 +25,7 @@ class Mesh1D():
             '-o',
             color="black",
         )
-        node_upper_list = np.array([self.cross_section_west / 2, *[display_node.cross_section / 2 for display_node in self.nodes], self.cross_section_east / 2])
+        node_upper_list = np.array([self.west_bc.cross_section / 2, *[display_node.cross_section / 2 for display_node in self.nodes], self.east_bc.cross_section / 2])
         node_lower_list = -node_upper_list
         plt.plot(
             x_position_list,
@@ -40,17 +41,50 @@ class Mesh1D():
         )
         plt.show()
 
+    def save_mesh(self, mesh_path: Path):
+        with mesh_path.open("w") as mesh_file:
+            mesh_file.write("node_id, x, area")
+            self.west_bc.to_csv(mesh_file)
+            for node in self.nodes:
+                node.to_csv(mesh_file)
+            self.east_bc.to_csv(mesh_file)
 
-def create_constant_section_mesh_1d(domain_length: float, cross_section: float, n_nodes: float):
-    dx, x_positions_mesh = equally_spaced_nodes(domain_length, n_nodes)
-
-    nodes = [Node1D(x_node, dx, cross_section) for x_node in x_positions_mesh]
-    return Mesh1D(nodes=nodes, cross_section_west=cross_section, cross_section_east=cross_section, domain_length=domain_length)
 
 
-def create_linear_section_mesh1D(domain_length: float, inlet_section: float, outlet_section: float, n_nodes:float):
-    dx, x_positions_mesh = equally_spaced_nodes(domain_length, n_nodes)
-    cross_sections_mesh = np.linspace(inlet_section, outlet_section, n_nodes)
 
-    nodes = [Node1D(x_node, dx, cross_section_node) for x_node, cross_section_node in zip(x_positions_mesh, cross_sections_mesh)]
-    return Mesh1D(nodes=nodes, cross_section_west=inlet_section, cross_section_east=outlet_section, domain_length=domain_length)
+def create_constant_section_mesh_1d(
+    domain_length: float,
+	cross_section: float,
+	n_nodes: float,
+) -> Mesh1D:
+    x_positions_mesh = equally_spaced_nodes(domain_length, n_nodes)
+    cross_sections_mesh = np.ones_like(x_positions_mesh) * cross_section
+    return create_custom_mesh1D(domain_length, cross_section, cross_section, x_positions_mesh, cross_sections_mesh)
+
+
+def create_linear_section_mesh1D(
+    domain_length: float,
+	inlet_section: float,
+	outlet_section: float,
+	n_nodes:float
+) -> Mesh1D:
+    x_positions_mesh = equally_spaced_nodes(domain_length, n_nodes)
+    # cross_sections_mesh = np.linspace(inlet_section, outlet_section, n_nodes)
+    cross_sections_mesh = inlet_section + equally_spaced_nodes((outlet_section - inlet_section), n_nodes)
+
+    return create_custom_mesh1D(domain_length, inlet_section, outlet_section, x_positions_mesh, cross_sections_mesh)
+
+def create_custom_mesh1D(
+        domain_length: float,
+        inlet_section: float,
+        outlet_section: float,
+        x_positions_mesh: np.array,
+        cross_sections_mesh: np.array
+    ) -> Mesh1D:
+    assert len(x_positions_mesh) == len(cross_sections_mesh), "x_position and cross_section arrays must have the same length"
+
+    west_bc = Node1D(-1, 0., inlet_section)
+    east_bc = Node1D(-1, domain_length, outlet_section)
+
+    nodes = [Node1D(node_id, x_node, cross_section_node) for node_id, (x_node, cross_section_node) in enumerate(zip(x_positions_mesh, cross_sections_mesh))]
+    return Mesh1D(nodes, west_bc, east_bc)
--- Mesh/node_1d.py
+++ Mesh/node_1d.py
@@ -1,10 +1,8 @@
 class Node1D():
-    def __init__(self, x: float, dx: float, cross_section: float):
+    def __init__(self, node_id: int, x: float, cross_section: float):
+        self.node_id = node_id
         self.x = x
-        self.dx = dx
         self.cross_section = cross_section
 
-        self.vars = dict()
-
-    def update_var(self, updated_var_name: str, updated_var_value: float):
-        self.vars[updated_var_name] = updated_var_value
+    def to_csv(self, mesh_file):
+        mesh_file.write(f"{self.node_id} {self.x} {self.cross_section}")
